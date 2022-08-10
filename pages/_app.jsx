25e8baecf872510f13441453b8cd1a4e52803f5f
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-08-10 20:26:45 +0530

    fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css';
 import '../styles/animations.css';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+function App({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
 
 export default App;
