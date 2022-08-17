6d5b2df15d6be47f287c819940ac66b455640054
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2022-08-17 15:20:38 +0530

    Added More Klasha Components
    Started working on campus ambassadors page with 2 components being made,
    can be viewed in /test route

---
+++ components/klasha components/CampusAmbassador.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeroSection from './HeroSection';
+import ImageDescription from './ImageDescription';
+import RoleComponent from './RoleComponent';
+
+const CampusAmbassador = () => {
+  return (
+    <div className="mt-20">
+      <HeroSection />
+      <RoleComponent />
+    </div>
+  );
+};
+
+export default CampusAmbassador;
