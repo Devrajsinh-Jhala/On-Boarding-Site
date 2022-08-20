3904b7f2a58581e928f266ca983d234bae259107
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2022-08-20 11:58:10 +0530

    Fixed some styling issues

--- components/klasha components/AccordianComponent.jsx
+++ components/klasha components/AccordianComponent.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
-import { Collapse } from 'react-collapse';
 
 const AccordianComponent = ({ question, answer }) => {
   const [isHidden, setIsHidden] = useState(true);
@@ -8,35 +7,26 @@ const AccordianComponent = ({ question, answer }) => {
     setIsHidden(!isHidden);
   };
   return (
-    <div className="mx-auto max-w-[872px] mb-8 ">
-      <div className=" outline-none border-b-[1px] border-black ">
+    <div className="mx-auto max-w-[872px] mb-8  ">
+      <div className=" outline-none border-b-[1px]  border-black ">
         {/* Question */}
         <div
           onClick={handleIsHidden}
-          className="flex justify-between items-center cursor-pointer py-6 transition-all duration-300 ease-in-out "
+          className="flex justify-between items-center cursor-pointer py-6 "
         >
-          <div className="max-w-[720px] text-24px pr-[50px] lg:pr-0 lg:text-[40px] tracking-[-0.02em] leading-[110%] ">
+          <div className="max-w-[720px] text-24px pr-[50px] lg:pr-0 lg:text-[40px] transition-all duration-1000 ease-in-out tracking-[-0.02em] leading-[110%] ">
             {question}
           </div>
           <div>{isHidden ? <BsChevronDown /> : <BsChevronUp />}</div>
         </div>
         {/* Answer */}
-        <Collapse
-          initialStyle={{
-            transition: 'height',
-            duration: '300ms',
-            transitionTimingFunction: 'ease-in-out',
-          }}
-          isOpened={true}
+        <div
+          className={`h-auto  transition-[height] mb-6 duration-500 ${
+            isHidden ? 'hidden' : 'block'
+          } ease-in-out `}
         >
-          <div
-            className={`h-auto  transition-[height] mb-6 duration-500 ${
-              isHidden ? 'hidden' : 'block'
-            } ease-in-out `}
-          >
-            <p className="mb-0 m-0 p-0 text-base leading-[160%] ">{answer}</p>
-          </div>
-        </Collapse>
+          <p className="mb-0 m-0 p-0 text-base leading-[160%] ">{answer}</p>
+        </div>
       </div>
     </div>
   );
